5debdf35adf740c7604c94701bb7fa5be86f292f
Taner Yener <taneryener@192.168.2.125>
2025-03-05 19:06:52 +0300

    removed node code format and lint in workflows

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
-use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Route;
 
-//api routes /api/***
+// api routes /api/***
 
 Route::get('/league', function () {
     return response()->json(['message' => 'league response']);
